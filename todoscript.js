3f0cf1ae946d673af8f6ccd630f5133120996c7f
consoleLogIt <kishansingh4do@gmail.com>
2020-06-21 19:03:44 +0530

    updated

---
+++ todoscript.js
@@ -0,0 +1,97 @@
+
+//task count
+var count =0;
+
+
+//selectors
+const taskCount = document.getElementById("#tasknumber");
+const todoInput = document.getElementById("add-item");
+const todoButton = document.getElementById("add-button");
+const todoList = document.getElementById("list");
+
+
+// event listners
+
+todoButton.addEventListener("click",addTodo);
+
+todoList.addEventListener("click",checkDelete);
+
+
+
+
+
+// functions
+
+function addTodo(){
+	if(todoInput.value==""){
+		alert("Empty task");
+	}
+	else{
+
+const todoDiv = document.createElement('div');
+	todoDiv.classList.add('todo');
+
+
+	const completedButton = document.createElement('input');
+    completedButton.type = "checkbox";
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton); 
+
+	                     
+	const newTodo = document.createElement('li');
+	newTodo.innerText = todoInput.value;
+	newTodo.classList.add('todo-item'); 
+	todoDiv.appendChild(newTodo);
+
+    
+
+    const deleteButton = document.createElement('span');
+    deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+    deleteButton.classList.add("trash-btn");
+    todoDiv.appendChild(deleteButton);
+
+
+    todoList.appendChild(todoDiv);
+    count++;
+    tasknumber.innerText = count+" tasks left";
+
+// clear input value
+}
+    todoInput.value = "";
+
+}
+
+
+
+function checkDelete(e){
+	const item = e.target;
+	
+	// console.log(item.classList);
+
+	if(item.classList[0]=="fas"){
+		const todo = item.parentElement.parentElement;
+		todo.remove();
+		count--;
+		tasknumber.innerHTML = count+" tasks left";
+	}
+
+if(item.classList[0]=="complete-btn"){
+		const todo = item.parentElement;
+		
+		const checker = todo.classList.toggle("done");
+	
+		if(checker==true){
+			count--;
+		}
+		else{
+			count++;
+		}
+		tasknumber.innerHTML = count+" tasks left";
+	}
+
+
+
+
+	
+	
+}
